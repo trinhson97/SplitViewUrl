60e3031e0b0ecae15fa964db63c11ea767027425
Son <trinhson1998@gmail.com>
2018-06-20 09:20:22 +0700

    Initial Commit

---
+++ SplitViewUrl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplitViewUrl
+//
+//  Created by Son on 2018/06/20.
+//  Copyright © 2018 son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
