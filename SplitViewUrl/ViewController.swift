285f1d447ecab504ea4f2b36d8f3d38ac9b9f4f2
Son <trinhson1998@gmail.com>
2018-06-28 08:12:51 +0700

    URL

--- SplitViewUrl/ViewController.swift
+++ SplitViewUrl/ViewController.swift
@@ -11,39 +11,48 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var removeSpinner: UIActivityIndicatorView!
     @IBOutlet weak var imageUrl: UIImageView!
-
-//    func imageLoad () {
-//        let loadUrl = "http://s1.picswalls.com/wallpapers/2016/06/10/4k-high-definition-wallpaper_065229159_309.jpg"
-//        self.removeSpinner.startAnimating()
-//        DispatchQueue.global().async {
-//            let url = URL(string: loadUrl)!
-//            do {
-//                let aData = try Data(contentsOf: url)
-//                DispatchQueue.main.async {
-//                    self.imageUrl.image = UIImage(data: aData)
-//                    self.removeSpinner.stopAnimating()
-//                }
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }
-//
-//    }
-
-
-    func imageLoad () {
-    let loadUrl = "http://s1.picswalls.com/wallpapers/2016/06/10/4k-high-definition-wallpaper_065229159_309.jpg"
-        imageUrl.download(from: loadUrl)
-
-    }
+    
+    var disPatchWorkItem: DispatchWorkItem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageLoad()
-    
+        removeSpinner.startAnimating()
+        fetchImage(from: "http://s1.picswalls.com/wallpapers/2016/06/10/4k-high-definition-wallpaper_065229159_309.jpg") { (image) in
+            self.imageUrl.image = image
+            self.removeSpinner.stopAnimating()
+            self.removeSpinner.isHidden = true
+        }
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        disPatchWorkItem?.cancel()
+    }
+    func fetchImage(from urlString: String, completedHandler: @escaping (UIImage?) -> Void ) {
+        guard let url = URL(string: urlString) else { return  }
+        var image: UIImage?
+        disPatchWorkItem = DispatchWorkItem(block: {
+            do {
+                if let cache = CacheImage.images.object(forKey: url.absoluteString as NSString) as? UIImage {
+                    image = cache
+                } else {
+                    let aData = try Data(contentsOf: url)
+                    image = UIImage(data: aData)
+                    CacheImage.images.setObject(image!, forKey: url.absoluteString as NSString, cost: aData.count)
+                }
+            }
+            catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        })
+        DispatchQueue.global().async {
+//            perform in GCD
+            self.disPatchWorkItem?.perform()
+            DispatchQueue.main.async {
+                completedHandler(image)
+            }
+        }
+    }
+  
 }
 
 
